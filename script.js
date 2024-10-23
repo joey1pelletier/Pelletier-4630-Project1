bb444ebb4f61aa7294a0922bf45cfc4de7638b36
Joseph Pelletier <143644756+joey1pelletier@users.noreply.github.com>
2024-10-23 17:58:18 -0600

    add cart success

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const items = document.querySelectorAll('.item');
 let item_num_target = document.getElementById('cart-num');
 console.log(item_num_target); 
-let item_counter = 0;
+
 
 let storedCart = sessionStorage.getItem('curr_cart')
 let cart = storedCart ? JSON.parse(storedCart) : [];
 
 item_num_target.innerText = cart.length;
+
+let total_cost_target = document.getElementById('total-cost');
+let total_cost = parseInt(sessionStorage.getItem('total')) || 0;
+total_cost_target.innerText = total_cost;
+
+let curr_item_info = document.getElementById('added-cart-items');
+const saved_cart_html = sessionStorage.getItem("cart_html");
+
+curr_item_info.innerHTML = saved_cart_html;
 
 items.forEach(item => {
     item.querySelector('.add-to-cart').addEventListener('click', () => {
@@ -17,15 +26,8 @@ items.forEach(item => {
     });
 });
 
-console.log(items.length);
-console.log(items[1].getAttribute('data-img'));
-
-//let cart = [];
-//let cart_num_display = document.getElementById('cart-num');
 sessionStorage.setItem("curr_cart_length", JSON.stringify(cart.length));
 let curr_cart_length_data = sessionStorage.getItem("curr_cart_length");
-//item_num_target.innerText = curr_cart_length_data;
-
 
 function addToCart(image, label, price) {
     const item = {
@@ -37,19 +39,74 @@ function addToCart(image, label, price) {
     console.log('Cart: ', cart);
 
     sessionStorage.setItem("curr_cart", JSON.stringify(cart));
-    //const curr_cart_data = sessionStorage.getItem("curr_cart");
     
 
     item_num_target.innerText = cart.length;
     sessionStorage.setItem("curr_cart_length", JSON.stringify(cart.length));
-    //curr_cart_length_data = sessionStorage.getItem("curr_cart_length");
-    //item_num_target.innerText = curr_cart_length_data;
-    
-/*
-    localStorage.setItem("curr_cart_length", JSON.stringify(cart.length));
-    const curr_cart_length_data = localStorage.getItem("curr_cart_length");
-    item_num_target.innerText = curr_cart_length_data;
-*/
 
+    const num_price = parseInt(price);
+    total_cost += num_price;
+    console.log(total_cost);
+
+    sessionStorage.setItem("total", total_cost);
+    total_cost_target.innerText = total_cost;
     
+    let item_info = document.getElementById('added-cart-items');
+
+
+    /*
+    example html code to be added
+
+    <div class="cart-item">
+        <img src="https://i.ibb.co/yfqXqtN/black-jacket.jpg" alt="black jacket" class="cart-item-img">
+        <p class="cart-item-label">Women's Black Jacket</p>
+        <p class="cart-item-price">$120</p>
+        <p>X</p>
+    </div>
+    */
+    let item_info_div = document.createElement('div');
+    item_info_div.className = 'cart-item';
+
+    let item_info_img = document.createElement('img');
+    item_info_img.src = image;
+    item_info_img.alt = label;
+    item_info_img.className = 'cart-item-img';
+
+    let item_info_label = document.createElement('p');
+    item_info_label.className = 'cart-item-label';
+    item_info_label.textContent = label;
+
+    let item_info_price = document.createElement('p');
+    item_info_price.className = 'cart-item-price';
+    item_info_price.textContent = `$${price}`;
+
+    let item_remove_icon = document.createElement('p');
+    item_remove_icon.textContent = 'X';
+
+    item_info_div.appendChild(item_info_img);
+    item_info_div.appendChild(item_info_label);
+    item_info_div.appendChild(item_info_price);
+    item_info_div.appendChild(item_remove_icon);
+
+    item_info.appendChild(item_info_div);
+
+    sessionStorage.setItem("cart_html", item_info.innerHTML);
+
+}
+
+let cart_button = document.getElementById('cart-area');
+let popup = document.getElementById('popup-cart');
+console.log(popup);
+cart_button.addEventListener("click", openPopup);
+
+let close_popup_button = document.getElementById('close-popup');
+close_popup_button.addEventListener("click", closePopup);
+
+function openPopup() {
+    popup.style.visibility = 'visible';
+
+}
+
+function closePopup() {
+    popup.style.visibility = 'hidden';
 }
